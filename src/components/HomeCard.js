14d5f70c62944175e2d9751df0966aa70b88e267
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-22 20:03:44 -0500

    minor styling

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -3,22 +3,43 @@ import "../App.css";
 
 const HomeCard = (props) => {
   return (
-    <div className="section-container">
-      <div class="ui segment">
-        <div>
-          {props.content}
+    <div>
+      <div className="section-container">
+        <div class="ui segment">
+          <div>
+            {props.content}
 
-          {/* <h1 class="ui left floated header title">Sam Munro</h1> */}
+            {/* <h1 class="ui left floated header title">Sam Munro</h1> */}
+          </div>
+          <div class="ui clearing divider"></div>
+          <button class="ui secondary button">Github</button>
+          <button class="ui secondary button">Projects</button>
+          {/* <button class="ui secondary button">Education</button> */}
+          <button class="ui secondary button">Resume</button>
+          <button class="ui linkedin button">
+            <i class="linkedin icon"></i>
+            LinkedIn
+          </button>
+          <br />
         </div>
-        <div class="ui clearing divider"></div>
-        <button class="ui secondary button">Github</button>
-        <button class="ui secondary button">Projects</button>
-        {/* <button class="ui secondary button">Education</button> */}
-        <button class="ui secondary button">Resume</button>
-        <button class="ui linkedin button">
-          <i class="linkedin icon"></i>
-          LinkedIn
-        </button>
+      </div>
+
+      <div className="ui move reveal">
+        Hey!
+        <div className="visible content">
+          <img
+            src="/images/wireframe/square-image.png"
+            className="ui small image"
+            alt="placeholder"
+          />
+        </div>
+        <div className="hidden content">
+          <img
+            src="/images/avatar/large/chris.jpg"
+            className="ui small image"
+            alt="avatar"
+          />
+        </div>
       </div>
     </div>
   );
