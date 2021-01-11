9c02ea17a8a15c5a7d1144f77f278cc8d354881e
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-11 11:32:47 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import NavigationBar from "./NavigationBar";
-import HomeCard from "./components/HomeCard";
-import SamCard from "./components/SamCard";
-
-import "./App.css";
 
 const App = () => {
   return (
-    <section className="main-container">
-      <NavigationBar logo={"Sam Munro"} />
-      <HomeCard content={<SamCard />} />
-    </section>
+    <div>
+      <div>Hello world</div>
+    </div>
   );
 };
 
